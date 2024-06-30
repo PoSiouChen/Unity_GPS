95f41e2c8b973562ff3cc0467910a383ab2c7d27
pollychen5911 <pollychen3911@gmail.com>
2024-06-30 22:39:16 +0800

    complete each frame

--- Assets/Scripts/ToggleText.cs
+++ Assets/Scripts/ToggleText.cs
@@ -5,11 +5,23 @@ using UnityEngine.UI;
 public class ToggleText : MonoBehaviour
 {
     public TextMeshProUGUI targetText;
+    public RectTransform arrowImage;
 
     public void OnButtonClick()
     {
-        if (targetText != null){
+        if (targetText != null)
+        {
             targetText.gameObject.SetActive(!targetText.gameObject.activeSelf);
-        } 
+        }
+
+        if (arrowImage != null)
+        {
+            arrowImage.localScale = new Vector3(
+                arrowImage.localScale.x,
+                arrowImage.localScale.y * -1, // 上下翻转
+                arrowImage.localScale.z
+            );
+        }
     }
 }
+
